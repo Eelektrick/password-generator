b24c1d0853c92541a03a7c1157a3762dca8644fc
Corey Post <corey.post0120@gmail.com>
2020-06-06 15:14:47 -0600

    updates to password generator function

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  
-  userChoice();
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -16,54 +14,68 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function userChoice(){
-  //confirm password length
-  var passLength = prompt("How long do you want your password to be? (can only use between 8 to 128 characters)");
-  if(passLength > 7 && passLength < 129){
-    alert("you chose a " + passLength + " character long password.") //validating the user choice
-    console.log(passLength)
-  }else{
-    alert("Sorry the length has to be within 8 to 128 characters")
-    return;
-  }
+function generatePassword(){
 
-  //confirm lower case letters and validating user choice
-  var lowerCase = confirm("Do you want lower case letters in your password?")
-  if(lowerCase === true){
-    alert("You chose to include lower case letters in your password.")
-    console.log(lowerCase)
-  }else{
-    alert("You chose not to include lower case letters in your password.")
-    console.log(lowerCase)
-  }
+  //variables for each array used
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "0123456789";
+  var symbol = "!@#$%^&()?`,.':/+-*";
+  var array = [lower, upper, number, symbol];
 
-  //confirm upper case letters and validating user choice
-  var upperCase = confirm("Do you want upper case letters in your password?")
-  if(upperCase === true){
-    alert("You chose to include upper case letters in your password.")
-    console.log(upperCase)
-  }else{
-    alert("You chose not to include upper case letters in your password.")
-    console.log(upperCase)
-  }
-
-  //confirm numeric characters and validating user choice
-  var numeric = confirm("Do you want numeric characters in your password?")
-  if(numeric === true){
-    alert("You chose to include numeric characters in your password.")
-    console.log(numeric)
-  }else{
-    alert("You chose not to include numeric characters in your password.")
-    console.log(numeric)
-  }
-
-  //confirm special characters and validating user choice
-  var symbols = confirm("Do you want special characters in your password?")
-  if(symbols === true){
-    alert("You chose to include special characters in your password.")
-    console.log(symbols)
-  }else{
-    alert("You chose not to include special characters in your password.")
-    console.log(symbols)
-  }
+   //confirm password length
+   var passLength = prompt("How long do you want your password to be? (can only use between 8 to 128 characters)");
+   if(passLength > 7 && passLength < 129){
+     alert("you chose a " + passLength + " character long password.") //validating the user choice
+     console.log(passLength)
+   }else{
+     alert("Sorry the length has to be within 8 to 128 characters")
+     return;
+   }
+ 
+   //confirm lower case letters and validating user choice
+   var lowerCase = confirm("Do you want lower case letters in your password?")
+   if(lowerCase === true){
+     alert("You chose to include lower case letters in your password.")
+     console.log(lowerCase)
+   }else{
+     alert("You chose not to include lower case letters in your password.")
+     console.log(lowerCase)
+   }
+ 
+   //confirm upper case letters and validating user choice
+   var upperCase = confirm("Do you want upper case letters in your password?")
+   if(upperCase === true){
+     alert("You chose to include upper case letters in your password.")
+     console.log(upperCase)
+   }else{
+     alert("You chose not to include upper case letters in your password.")
+     console.log(upperCase)
+   }
+ 
+   //confirm numeric characters and validating user choice
+   var numeric = confirm("Do you want numeric characters in your password?")
+   if(numeric === true){
+     alert("You chose to include numeric characters in your password.")
+     console.log(numeric)
+   }else{
+     alert("You chose not to include numeric characters in your password.")
+     console.log(numeric)
+   }
+ 
+   //confirm special characters and validating user choice
+   var symbols = confirm("Do you want special characters in your password?")
+   if(symbols === true){
+     alert("You chose to include special characters in your password.")
+     console.log(symbols)
+   }else{
+     alert("You chose not to include special characters in your password.")
+     console.log(symbols)
+   }
+ 
+  //  //for loop to create password and return to writePassword
+  //  for(var i = 0; i<passLength; i++){
+  //    //random choices for password from arrays
+  //    var passCreated = 
+  //  }
 }
