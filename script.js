c44c5eb91ad9521a24b9effcac945268cf03dc90
Corey Post <corey.post0120@gmail.com>
2020-06-08 22:28:33 -0600

    more updates to if else statements to create password

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function generatePassword(){
   var lower = "abcdefghijklmnopqrstuvwxyz";
   var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var number = "0123456789";
-  var symbol = "!@#$%^&()?`,.':/+-*";
+  var symbolAnswer = "!@#$%^&()[|]~?`,.':/+-*";
   var passCreate = "";
   var passFinal = "";
 
@@ -38,6 +38,7 @@ function generatePassword(){
   //confirm lower case letters and validating user choice
   var lowerCase = confirm("Do you want lower case letters in your password?")
   if(lowerCase === true){
+    passCreate += lower;
     alert("You chose to include lower case letters in your password.")
     console.log(lowerCase)
   }
@@ -46,10 +47,10 @@ function generatePassword(){
     console.log(lowerCase)
   }
 
- 
   //confirm upper case letters and validating user choice
   var upperCase = confirm("Do you want upper case letters in your password?")
   if(upperCase === true){
+    passCreate += upper;
     alert("You chose to include upper case letters in your password.")
     console.log(upperCase)
   }
@@ -61,6 +62,7 @@ function generatePassword(){
   //confirm numeric characters and validating user choice
   var numeric = confirm("Do you want numeric characters in your password?")
   if(numeric === true){
+    passCreate += number;
     alert("You chose to include numeric characters in your password.")
     console.log(numeric)
   }
@@ -72,6 +74,7 @@ function generatePassword(){
   //confirm special characters and validating user choice
   var symbols = confirm("Do you want special characters in your password?")
   if(symbols === true){
+    passCreate += symbolAnswer;
     alert("You chose to include special characters in your password.")
     console.log(symbols)
   }
